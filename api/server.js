af82ba819655ae3292cc75325787a5b426566b32
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-08 17:47:50 -0800

    Fix error
    Set main to server.js instead of index.js

--- api/server.js
+++ api/server.js
@@ -14,4 +14,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/mern-todo', {
     .then(() => console.log('Connected to DB'))
     .catch(console.error);
 
-app.listen(3001, () => console.log('Server started on port 3001'))
+app.listen(3001, () => console.log('Server started on port 3001'));
