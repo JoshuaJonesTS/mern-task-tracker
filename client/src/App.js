ba5597f7b498795409408e18f103e9afa4385b8f
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-09 17:37:09 -0800

    Render sample headers onto React application

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome, Tyler</h1>
+      <h4>Your Task</h4>
+
+      <div className="todos">
+        <div classname="todo">
+          <div classnName="checkbox"></div>
+          <div className="text">Get the bread</div>
+          <div className="delete-todo"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
