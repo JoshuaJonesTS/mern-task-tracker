c5f8c738f1a0a929052e7d2448a8eb32253f3641
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-09 18:49:55 -0800

    Dynamically render a todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+
+    console.log(todos);
+  }, []);
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+    .then(res => res.json())
+    .then(data => setTodos(data))
+    .catch(err => console.error("Error: ", err))
+  }
+
   return (
     <div className="App">
       <h1>Welcome, Tyler</h1>
       <h4>Your Task</h4>
 
       <div className="todos">
-        <div classname="todo">
-          <div classnName="checkbox"></div>
-          <div className="text">Get the bread</div>
-          <div className="delete-todo"></div>
-        </div>
-        <div classname="todo is-complete">
-          <div classnName="checkbox"></div>
-          <div className="text">Get the milk</div>
-          <div className="delete-todo"></div>
-        </div>
+        {
+          todos.map(todo => (
+            <div classname="todo">
+              <div classnName="checkbox"></div>
+              <div className="text">{ todo.text }</div>
+              <div className="delete-todo"></div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
