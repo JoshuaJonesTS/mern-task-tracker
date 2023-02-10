e891d7399100d6e3f8ab5fae74e32c6a7eef32b1
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-09 19:29:42 -0800

    Add key to todo

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
       <div className="todos">
         {
           todos.map(todo => (
-            <div classname="todo">
+            <div classname="todo" key={todo._id}>
               <div classnName="checkbox"></div>
               <div className="text">{ todo.text }</div>
               <div className="delete-todo"></div>
