97462960bc08db09c71edb8e1eca7078defa4ed5
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-02-10 18:43:59 -0800

    Allow addition of new task

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,22 @@ function App() {
     setTodos(todos => todos.filter(todo => todo._id !== data._id));
   }
 
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json());
+    
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  }
+
   return (
     <div className="App">
       <h1>Welcome, Tyler</h1>
@@ -57,9 +73,24 @@ function App() {
             onClick={() => completeTodo(todo._id)}>
               <div classnName="checkbox"></div>
               <div className="text">{ todo.text }</div>
-              <div 
-                className="delete-todo" onClick={() => deleteTodo(todo._id)}>
-              </div>
+              <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
+              <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+              {
+                popupActive ? (
+                  <div className="popup">
+                    <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+                    <div className="content">
+                      <h3>Add Task</h3>
+                      <input
+                        type="text"
+                        className="add-todo-input"
+                        onChange={e => setNewTodo(e.target.value)}
+                        value={newTodo}
+                      />
+                      <div className="button" onClick={addTodo}>Create Task</div>
+                    </div>
+                  </div>
+                ): ''}
             </div>
           ))
         }
