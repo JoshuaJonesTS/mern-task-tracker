6225bfb8b0cbed11177ead4b4ee19ea16988f953
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-04 22:00:06 -0800

    Make display bold

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_DB);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.brightBlue.underline.italic);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.brightBlue.underline.bold);
 }
 
 module.exports = connectDB;
