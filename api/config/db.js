26208431969ecc2f754c82c27862926b85453b2b
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-04 21:54:31 -0800

    Make style database connection confirmation

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_DB);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.brightBlue.underline.italic);
 }
 
 module.exports = connectDB;
