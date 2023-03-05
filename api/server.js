cdb244a9accb808dde5aedb3cd4e2a03da6063fe
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-04 20:14:03 -0800

    Update server.js
    Cease using the default database as it no longer works. Create a new database in MongoDB Atlas. Connect to said database.

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+mongoose.connect(process.env.MONGO_DB, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
